e84a7e884f92cc8dfe892e213f107e6483967d62
CHECH01 <ss_1913@ooutlook.com>
2020-11-12 11:32:33 -0500

    refactor

--- src/tictactoe/Board.java
+++ src/tictactoe/Board.java
@@ -3,37 +3,9 @@ package tictactoe;
 public class Board {
 	private String[][] board 	= { { "A", "B", "C" }, { "D", "E", "F" }, { "G", "H", "I" } };
 	private String[][] guiBoard = new String[9][17];
-
-	public void printBoard() {
-		for (int i = 0; i <= 8; i++) {
-			for (int j = 0; j <= 16; j++)
-				System.out.print(guiBoard[i][j]);
-			System.out.println("");
-		}
-	}
-
-	public boolean isWinner() {
-		for (int i = 0; i <= 2; i++)
-			for (int j = 0; j <= 2; j++) {
-				if ((i == 0 && j == 0) || (i == 1 && j == 0) || (i == 2 && j == 0)) {// horizontal
-					if (board[i][j].contentEquals(board[i][j + 1]) && board[i][j].contentEquals(board[i][j + 2]))
-						return true;
-				}
-				if ((i == 0 && j == 0) || (i == 0 && j == 1) || (i == 0 && j == 2)) {// vertical
-					if (board[i][j].contentEquals(board[i + 1][j]) && board[i][j].contentEquals(board[i + 2][j]))
-						return true;
-				}
-				if ((i == 1 && j == 1)) {// diagonal
-					if (board[i][j].contentEquals(board[i - 1][j - 1])
-							&& board[i][j].contentEquals(board[i + 1][j + 1]))
-						return true;
-					else if (board[i][j].contentEquals(board[i - 1][j + 1])
-							&& board[i][j].contentEquals(board[i + 1][j - 1]))
-						return true;
-				}
-
-			}
-		return false;
+	
+	public Board() {
+		generateEmptyBoard();
 	}
 
 	public void mark(String pos, String mark) {
@@ -66,5 +38,13 @@ public class Board {
 		guiBoard[7][2] 	= "G";
 		guiBoard[7][8] 	= "H";
 		guiBoard[7][14] = "I";
+	}
+	
+	public String[][] getBoard() {
+		return board;
+	}
+
+	public String[][] getGuiBoard() {
+		return guiBoard;
 	}
 }
--- src/tictactoe/Main.java
+++ src/tictactoe/Main.java
@@ -1,74 +1,8 @@
 package tictactoe;
 
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Main {
-	
-	private static Scanner input;
-	ArrayList<String> myArray = new ArrayList<String>();
 	public static void main(String[] args) {
-		input = new Scanner(System.in);
-		Main tictactoe = new Main();
-		tictactoe.initializeArray();
-		Board myBoard = new Board();
-		myBoard.generateEmptyBoard();
-		myBoard.printBoard();
-		tictactoe.play(myBoard);
-	}
-	public void play(Board myBoard) {	
-		String mark = "";
-		int aux = 1;
-		boolean isWinner = false;
-	
-		do {
-			if(aux % 2 == 0) {
-				mark = "O";
-				System.out.print("Player O move: ");
-			}else {
-				mark = "X";
-				System.out.print("Player X move: ");
-			}
-			myBoard.mark(checkMark(),mark);
-			myBoard.printBoard();
-			isWinner = myBoard.isWinner();
-			aux++;
-		}while(isWinner == false && aux < 10);
-		
-		if(aux == 10 && isWinner == false)
-			System.out.print("DRAW!");
-		else
-			System.out.print(mark+" WINNER!!!");	
-	}
-	public String checkMark() {
-		String pos = "";
-		boolean aux = false;
-		pos = input.nextLine();
-		do {
-			for(int i = 0 ; i < myArray.size();i++)
-				if(pos.contentEquals(myArray.get(i))) {
-					aux = true;
-					myArray.remove(myArray.indexOf(pos));
-					break;
-				}
-			if(aux == false) {
-				System.out.print("Wrong move, try again: ");
-				pos = input.nextLine();
-			}
-				
-		}while(aux == false);
-		
-		return pos;
-	}
-	public void initializeArray() {
-		myArray.add("A");
-		myArray.add("B");
-		myArray.add("C");
-		myArray.add("D");
-		myArray.add("E");
-		myArray.add("F");
-		myArray.add("G");
-		myArray.add("H");
-		myArray.add("I");
+		TicTacToe myTicTacToe = new TicTacToe();
+		myTicTacToe.play();
 	}
 }
